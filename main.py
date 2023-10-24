5539153c2bae4486345e3046dffb251af95238fd
Yioy <seb.doutr@gmail.com>
2023-10-24 18:44:55 +0200

    Final commit

--- main.py
+++ main.py
@@ -100,8 +100,8 @@ validation_dataset = validationSet.cache().prefetch(buffer_size=AUTOTUNE)
 #create architecture of the model
 model = tf.keras.Sequential([
   tf.keras.layers.Rescaling(1./255),
-  tf.keras.layers.Conv2D(16, 3, activation='relu'),
-  tf.keras.layers.MaxPooling2D(2),
+  tf.keras.layers.Conv2D(16, (3,3), activation='relu'),
+  tf.keras.layers.MaxPooling2D((2,2)),
   tf.keras.layers.Dropout(0.5),
   tf.keras.layers.Flatten(),
   tf.keras.layers.Dense(64, activation='relu'),
